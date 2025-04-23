895fa6f2cafa6d7e5ab62b30a797864221d2793d
josealdo camogliano <camogliano.97@gmail.com>
2025-04-23 12:51:30 -0500

    v3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2029743324"
+    serviceWorkerVersion: "262452650"
   }
 });
